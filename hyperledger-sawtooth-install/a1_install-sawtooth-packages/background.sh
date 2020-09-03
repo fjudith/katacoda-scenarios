b17c36f1a8640baea69d88e7ef2e2db1d53cdb24
Florian JUDITH <florian.judith.b@gmail.com>
2020-09-03 22:37:13 +0200

    fixed background

--- hyperledger-sawtooth-install/a1_install-sawtooth-packages/background.sh
+++ hyperledger-sawtooth-install/a1_install-sawtooth-packages/background.sh
@@ -9,21 +9,6 @@ wait_file() {
   ((++wait_seconds))
 }
 
-sudo grep -i "done" /opt/katacoda-background-finished &> /dev/null
-if [[ "$?" -eq 0 ]]; then
-exit
-fi
-
-while true; do sudo grep -i "done" /opt/katacoda-finished > /dev/null && break || sleep 2; done
-
-echo "Everything ready... Finalise the deployment"
-
-/usr/local/bin/deploy.sh
-
-echo "done" | sudo tee /root/katacoda-background-finished
-
-
-
 wait_file docker-compose.yaml
 wait_file Dockerfile
 
