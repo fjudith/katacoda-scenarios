3f43185ebd74e04520c8b6ed2402353407715879
Florian JUDITH <florian.judith.b@gmail.com>
2020-09-05 17:55:27 +0200

    fixed service name

--- hyperledger-sawtooth-install/a6_verify-install-and-fix-if-necessary/assets/assets/chsa-a6-02/docker-entrypoint.sh
+++ hyperledger-sawtooth-install/a6_verify-install-and-fix-if-necessary/assets/assets/chsa-a6-02/docker-entrypoint.sh
@@ -9,6 +9,6 @@ fi
 sudo systemctl enable sawtooth-validator
 sudo systemctl enable sawtooth-settings-tp
 sudo systemctl enable sawtooth-rest-api
-sudo systemctl enable sawtooth-intkey-tp
+sudo systemctl enable sawtooth-intkey-tp-python
 
 exec "$@"
