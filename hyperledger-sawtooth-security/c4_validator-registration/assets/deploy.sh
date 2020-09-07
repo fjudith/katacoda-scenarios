98c0b6f625b19805c41abcd5b22bbb5198874871
Florian JUDITH <florian.judith.b@gmail.com>
2020-09-07 15:50:36 +0200

    Fixed weave-scope

--- hyperledger-sawtooth-security/c4_validator-registration/assets/deploy.sh
+++ hyperledger-sawtooth-security/c4_validator-registration/assets/deploy.sh
@@ -2,14 +2,14 @@
 
 cd /root/
 
-docker container run \
---name=scope \
--d \
---net=host \
---pid=host \
---privileged \
--v /var/run/docker.sock:/var/run/docker.sock:rw \
-weaveworks/scope:1.13.1 --probe.docker=true
+# docker container run \
+# --name=scope \
+# -d \
+# --net=host \
+# --pid=host \
+# --privileged \
+# -v /var/run/docker.sock:/var/run/docker.sock:rw \
+# weaveworks/scope:1.13.1 --probe.docker=true
 
 /usr/local/bin/docker-compose pull
 
