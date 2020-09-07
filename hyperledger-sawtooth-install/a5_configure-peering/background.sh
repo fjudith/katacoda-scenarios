43c522cdf8d2f07d358d8e29bf15e77b90a2107d
Florian JUDITH <florian.judith.b@gmail.com>
2020-09-07 13:41:04 +0200

    Background script only wait for 1 second

--- hyperledger-sawtooth-install/a5_configure-peering/background.sh
+++ hyperledger-sawtooth-install/a5_configure-peering/background.sh
@@ -4,7 +4,7 @@ wait_file() {
   local file="$1"; shift
   local wait_seconds="${1:-10}"; shift # 10 seconds as default timeout
 
-  until test $((wait_seconds--)) -eq 0 -o -e "$file" ; do sleep 2; done
+  until test $((wait_seconds--)) -eq 0 -o -e "$file" ; do sleep 1; done
 
   ((++wait_seconds))
 }
