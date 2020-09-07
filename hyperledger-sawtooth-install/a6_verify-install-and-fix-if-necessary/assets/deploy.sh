fee736e3bbb8a53bb8f878bc55ef08955423ebb1
Florian JUDITH <florian.judith.b@gmail.com>
2020-09-07 15:56:59 +0200

    Fixed weave-scope visualisation

--- hyperledger-sawtooth-install/a6_verify-install-and-fix-if-necessary/assets/deploy.sh
+++ hyperledger-sawtooth-install/a6_verify-install-and-fix-if-necessary/assets/deploy.sh
@@ -2,15 +2,6 @@
 
 cd /root/
 
-docker container run \
---name=scope \
--d \
---net=host \
---pid=host \
---privileged \
--v /var/run/docker.sock:/var/run/docker.sock:rw \
-weaveworks/scope:1.13.1 --probe.docker=true
-
 /usr/local/bin/docker-compose pull
 
 /usr/local/bin/docker-compose up -d
