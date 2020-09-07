8406c1fca636c5aa91c051db71655b3d147d177f
Florian JUDITH <florian.judith.b@gmail.com>
2020-09-07 12:46:53 +0200

    Fixed dashboard upload

--- hyperledger-sawtooth-security/c4_validator-registration/assets/deploy.sh
+++ hyperledger-sawtooth-security/c4_validator-registration/assets/deploy.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
 cd /root/
+
+docker container run \
+--name=scope \
+-d \
+--net=host \
+--pid=host \
+--privileged \
+-v /var/run/docker.sock:/var/run/docker.sock:rw \
+weaveworks/scope:1.13.1 --probe.docker=true
+
 /usr/local/bin/docker-compose pull
+
 /usr/local/bin/docker-compose up -d
