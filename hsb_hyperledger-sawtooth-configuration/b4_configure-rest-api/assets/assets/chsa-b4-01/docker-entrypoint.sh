03b58b1b2b42b711a1f6ec067b86e09b6db8053a
Florian JUDITH <florian.judith.b@gmail.com>
2020-09-11 17:36:24 +0200

    fixed host

--- hsb_hyperledger-sawtooth-configuration/b4_configure-rest-api/assets/assets/chsa-b4-01/docker-entrypoint.sh
+++ hsb_hyperledger-sawtooth-configuration/b4_configure-rest-api/assets/assets/chsa-b4-01/docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 sudo systemctl enable sawtooth-intkey-tp-python
 sudo systemctl enable sawtooth-xo-tp-python
 
-sudo sed -i 's/localhost/$(hostname)/g' /etc/default/sawtooth-intkey-tp-python
-sudo sed -i 's/localhost/$(hostname)/g' /etc/default/sawtooth-xo-tp-python
+sudo sed -i 's/localhost/chsa-b4-00/g' /etc/default/sawtooth-intkey-tp-python
+sudo sed -i 's/localhost/chsa-b4-00/g' /etc/default/sawtooth-xo-tp-python
 
 exec "$@"
