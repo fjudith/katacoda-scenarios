e04b2abdd4284f497d14c640eaa12137f9301b41
Florian JUDITH <florian.judith.b@gmail.com>
2020-09-12 13:15:04 +0200

    Fixed entrypoint

--- hsb_hyperledger-sawtooth-configuration/b7_configure-transaction-processor-endpoint/assets/assets/chsa-b7-01/docker-entrypoint.sh
+++ hsb_hyperledger-sawtooth-configuration/b7_configure-transaction-processor-endpoint/assets/assets/chsa-b7-01/docker-entrypoint.sh
@@ -5,7 +5,4 @@ set -e
 sudo systemctl enable sawtooth-intkey-tp-python
 sudo systemctl enable sawtooth-xo-tp-python
 
-sudo sed -i 's/localhost/chsa-b4-00/g' /etc/default/sawtooth-intkey-tp-python
-sudo sed -i 's/localhost/chsa-b4-00/g' /etc/default/sawtooth-xo-tp-python
-
 exec "$@"
