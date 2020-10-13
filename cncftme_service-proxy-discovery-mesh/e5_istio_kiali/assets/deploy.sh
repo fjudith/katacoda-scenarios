46c06bd098bc3e6ea580771af09b90b20f7b748b
Florian JUDITH <florian.judith.b@gmail.com>
2020-10-14 01:51:58 +0200

    Leverage istio operator installation

--- cncftme_service-proxy-discovery-mesh/e5_istio_kiali/assets/deploy.sh
+++ cncftme_service-proxy-discovery-mesh/e5_istio_kiali/assets/deploy.sh
@@ -7,7 +7,17 @@ ISTIO_VERSION="1.7.3"
 curl -L https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istio-${ISTIO_VERSION}-linux-amd64.tar.gz | tar xvzf -
 chmod +x istio-${ISTIO_VERSION}/bin/istioctl
 sudo ln -s ${PWD}/istio-${ISTIO_VERSION}/bin/istioctl /usr/local/bin/istioctl
-istioctl install --set profile=demo --force
+istioctl operator init
+kubectl create ns istio-system
+kubectl apply --wait -f - <<EOF
+apiVersion: install.istio.io/v1alpha1
+kind: IstioOperator
+metadata:
+  namespace: istio-system
+  name: example-istiocontrolplane
+spec:
+  profile: demo
+EOF
 
 # BookInfo
 kubectl label namespace default isio-injection=true
