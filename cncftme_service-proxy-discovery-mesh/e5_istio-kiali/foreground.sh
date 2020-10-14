04c5ea334bfe5a0b690af667922e0e184b2c8d46
Florian JUDITH <florian.judith.b@gmail.com>
2020-10-14 23:03:00 +0200

    Reduced verbosity

--- cncftme_service-proxy-discovery-mesh/e5_istio-kiali/foreground.sh
+++ cncftme_service-proxy-discovery-mesh/e5_istio-kiali/foreground.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-echo "Deploying lab components:"
-echo " "
-echo "|----------+--------------------|"
-echo "| Istio    | service mesh       |"
-echo "|----------+--------------------|"
-echo "| Contour  | ingress controller |"
-echo "|----------+--------------------|"
-echo "| Bookinfo | sample application |"
-echo "|----------+--------------------|"
-
-sleep 2
-
-wait.sh
+echo "Deploying lab components: Istio, Contour, Bookinfo"
+sleep 2; wait.sh
