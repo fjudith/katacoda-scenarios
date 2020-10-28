e297b64210b2c5a25f9b8910466818096a858461
Florian JUDITH <florian.judith.b@gmail.com>
2020-10-28 17:06:59 +0100

    Suppress error messages

--- cncftmb_ci-cd/b1_argo-workflows-basics/foreground.sh
+++ cncftmb_ci-cd/b1_argo-workflows-basics/foreground.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Wait for controlplane"
-until kubectl get node controlplane; do printf '.' && sleep 5 ; done && \
+until kubectl get node controlplane > /dev/null 2>&1 ; do printf '.' && sleep 5 ; done && \
 kubectl wait node/controlplane --for=condition=Ready --timeout "2m"
 
 echo "Wait for node01"
-until kubectl get node node01; do printf '.' && sleep 5 ; done && \
+until kubectl get node node01 > /dev/null 2>&1 ; do printf '.' && sleep 5 ; done && \
 kubectl wait node/node01 --for=condition=Ready --timeout "2m"
 
 echo "Deploying lab components: Helm, OpenEBS, Contour"
