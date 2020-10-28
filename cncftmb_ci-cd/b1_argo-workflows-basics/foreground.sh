5405139050553e41cd42ceceae5c382dee723b5a
Florian JUDITH <florian.judith.b@gmail.com>
2020-10-28 17:21:14 +0100

    Moved k8s nodes wait to wait.sh

--- cncftmb_ci-cd/b1_argo-workflows-basics/foreground.sh
+++ cncftmb_ci-cd/b1_argo-workflows-basics/foreground.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-set -x
-
-echo "Wait for controlplane"
-until kubectl get node controlplane > /dev/null 2>&1 ; do printf '.' && sleep 5 ; done && \
-kubectl wait node/controlplane --for=condition=Ready --timeout "2m"
-
-echo "Wait for node01"
-until kubectl get node node01 > /dev/null 2>&1 ; do printf '.' && sleep 5 ; done && \
-kubectl wait node/node01 --for=condition=Ready --timeout "2m"
-
 echo "Deploying lab components: Helm, OpenEBS, Contour"
 
 sleep 2; wait.sh
-
-set +x
