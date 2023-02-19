b40ff3c0aa40fe1b8fb78e2f4d311bd28c0fbde3
Florian JUDITH <florian.judith.b@gmail.com>
2023-02-19 00:01:08 -0500

    Fixed lun id

--- LFS-essential-commands/01-log-into-local-remote-graphical-and-text-mode-consoles/assets/scripts/install_iscsi.sh
+++ LFS-essential-commands/01-log-into-local-remote-graphical-and-text-mode-consoles/assets/scripts/install_iscsi.sh
@@ -11,6 +11,7 @@ DISK_AMOUNT=12
 for i in $(seq 1 ${DISK_AMOUNT})
 do
     disk_id="$(printf "%03d" ${i})"
+    lun_name="lun$((${i} - 1))"
     # assume we have want to store our iscsi disk image
     # at /var/lib/devices and use a 10G disk.
     # create the disk image
@@ -19,8 +20,8 @@ do
     # configure iscsi tgt
     # we use CHAP authentication (bidirectional) and set the initiator 
     # address to only allow localhost to enhance iscsi security
-    cat <<EOF | tee -a /etc/tgt/conf.d/katacoda_iscsi.conf
-<target iqn.0000-00.node-001.local:katacoda>
+    cat <<EOF | tee -a /etc/tgt/conf.d/killercoda_iscsi.conf
+<target iqn.0000-00.target.local:${lun_name}>
   backing-store /var/lib/devices/disk-${disk_id}.img
   initiator-address 127.0.0.1
   incominguser iscsi-user random_password
@@ -39,9 +40,10 @@ tgtadm --mode target --op show
 for i in $(seq 1 ${DISK_AMOUNT})
 do
     disk_id="$(printf "%03d" ${i})"
+    lun_name="lun$((${i} - 1))"
     # configure iscsi initiator
     iscsiadm -m discovery -t st -p 127.0.0.1
-    cat <<EOF | tee -a /etc/iscsi/nodes/iqn.0000-00.node-${disk_id}.local\:katacoda/127.0.0.1\,3260\,1/default
+    cat <<EOF | tee -a "/etc/iscsi/nodes/iqn.0000-00.target.local\:${lun_name}/127.0.0.1\,3260\,1/default"
 node.session.auth.authmethod = CHAP
 node.session.auth.username = iscsi-user
 node.session.auth.password = random_password
@@ -50,7 +52,7 @@ node.session.auth.password_in = random_password_out
 EOF
 
   # enable automatic startup and the systemd service
-  sed -i 's/node.startup = manual/node.startup = automatic/g' /etc/iscsi/nodes/iqn.0000-00.node-${disk_id}.local\:katacoda/127.0.0.1\,3260\,1/default
+  sed -i 's/node.startup = manual/node.startup = automatic/g' /etc/iscsi/nodes/iqn.0000-00.node-${disk_id}.local\:killercoda/127.0.0.1\,3260\,1/default
 done
 
 systemctl enable open-iscsi
