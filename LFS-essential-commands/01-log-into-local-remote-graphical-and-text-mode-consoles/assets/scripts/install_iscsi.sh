82f81db3f54f8db04a1cefe3f65b94f7ba6c6c8b
Florian JUDITH <florian.judith.b@gmail.com>
2023-02-19 18:02:38 -0500

    Use functions to orchestrate configuration

--- LFS-essential-commands/01-log-into-local-remote-graphical-and-text-mode-consoles/assets/scripts/install_iscsi.sh
+++ LFS-essential-commands/01-log-into-local-remote-graphical-and-text-mode-consoles/assets/scripts/install_iscsi.sh
@@ -14,22 +14,25 @@ INCOMING_PASSWORD='random_password'
 OUTGOING_USER='iscsi-target'
 OUTGOING_PASSWORD='random_password_out'
 
-for i in $(seq 1 ${DISK_COUNT})
-do
-  disk_id="$(printf "%03d" ${i})"
-  lun_name="lun$((${i} - 1))"
-  # assume we have want to store our iscsi disk image
-  # at /var/lib/devices and use a 10G disk.
-  # create the disk image
-  sudo mkdir -p /var/lib/devices
-  sudo fallocate -l 1G /var/lib/devices/disk-${disk_id}.img \
-  || logger -p syslog.error -p local0.error \
-     "Failed to create disk image \"/var/lib/devices/disk-${disk_id}.img\""
 
-  # configure iscsi tgt
-  # we use CHAP authentication (bidirectional) and set the initiator 
-  # address to only allow localhost to enhance iscsi security
-  cat <<EOF | sudo tee -a /etc/tgt/conf.d/killercoda_iscsi.conf
+function configure_target()
+{
+  for i in $(seq 1 ${DISK_COUNT})
+  do
+    disk_id="$(printf "%03d" ${i})"
+    lun_name="lun$((${i} - 1))"
+    # assume we have want to store our iscsi disk image
+    # at /var/lib/devices and use a 10G disk.
+    # create the disk image
+    sudo mkdir -p /var/lib/devices
+    sudo fallocate -l 1G /var/lib/devices/disk-${disk_id}.img \
+    || logger -p syslog.error -p local0.error \
+      "Failed to create disk image \"/var/lib/devices/disk-${disk_id}.img\""
+
+    # configure iscsi tgt
+    # we use CHAP authentication (bidirectional) and set the initiator 
+    # address to only allow localhost to enhance iscsi security
+    cat <<EOF | sudo tee -a /etc/tgt/conf.d/killercoda_iscsi.conf
 <target ${TARGET_IQN}:${lun_name}>
   backing-store /var/lib/devices/disk-${disk_id}.img
   initiator-address ${TARGET_ADDRESS}
@@ -37,9 +40,9 @@ do
   outgoinguser ${OUTGOING_USER} ${OUTGOING_PASSWORD}
 </target>
 EOF
-done
+  done
 
-cat <<EOF | sudo tee /etc/systemd/system/tgt.service
+  cat <<EOF | sudo tee /etc/systemd/system/tgt.service
 [Unit]
 Description=(i)SCSI target daemon
 Documentation=man:tgtd(8)
@@ -70,25 +73,29 @@ Restart=on-failure
 WantedBy=multi-user.target
 EOF
 
-# enable iscsi tgt service
-sudo systemctl daemon-reload
-sudo systemctl enable tgt.service
-sudo systemctl restart tgt.service
+  # enable iscsi tgt service
+  sudo systemctl daemon-reload
+  sudo systemctl enable tgt.service
+  sudo systemctl restart tgt.service
 
-sleep 5s
+  sleep 5s
 
-# optionally, verify the target is working
-sudo tgtadm --mode target --op show
+  # optionally, verify the target is working
+  sudo tgtadm --mode target --op show
+}
 
-# generate node configuration files
-sudo iscsiadm -m discovery -t st -p ${TARGET_ADDRESS}:${TARGET_PORT}
 
-for i in $(seq 1 ${DISK_COUNT})
-do
-  disk_id="$(printf "%03d" ${i})"
-  lun_name="lun$((${i} - 1))"
-  # configure iscsi initiator
-  cat <<EOF | tee -a /etc/iscsi/nodes/${TARGET_IQN}\:${lun_name}/${TARGET_ADDRESS}\,${TARGET_PORT}\,1/default
+function configure_initiator()
+{
+  # generate node configuration files
+  sudo iscsiadm -m discovery -t st -p ${TARGET_ADDRESS}:${TARGET_PORT}
+
+  for i in $(seq 1 ${DISK_COUNT})
+  do
+    disk_id="$(printf "%03d" ${i})"
+    lun_name="lun$((${i} - 1))"
+    # configure iscsi initiator
+    cat <<EOF | tee -a /etc/iscsi/nodes/${TARGET_IQN}\:${lun_name}/${TARGET_ADDRESS}\,${TARGET_PORT}\,1/default
 node.session.auth.authmethod = CHAP
 node.session.auth.username = ${INCOMING_USER}
 node.session.auth.password = ${INCOMING_PASSWORD}
@@ -96,42 +103,54 @@ node.session.auth.username_in = ${OUTGOING_USER}
 node.session.auth.password_in = ${OUTGOING_PASSWORD}
 EOF
 
-  # enable automatic startup and the systemd service
-  sed -i 's/node.startup = manual/node.startup = automatic/g' /etc/iscsi/nodes/${TARGET_IQN}\:${lun_name}/${TARGET_ADDRESS}\,${TARGET_PORT}\,1/default
-done
+    # enable automatic startup and the systemd service
+    sed -i 's/node.startup = manual/node.startup = automatic/g' /etc/iscsi/nodes/${TARGET_IQN}\:${lun_name}/${TARGET_ADDRESS}\,${TARGET_PORT}\,1/default
+  done
 
-sudo systemctl daemon-reload
-sudo systemctl enable open-iscsi
-sudo systemctl restart open-iscsi
-
+  sudo systemctl daemon-reload
+  sudo systemctl enable open-iscsi
+  sudo systemctl restart open-iscsi
+}
 
 # wait for open-iscsi to create virtual devices
 function find_devs()
 {
   find /dev/disk/by-path/ -name "ip-${TARGET_ADDRESS}\:${TARGET_PORT}-iscsi-${TARGET_IQN}\:lun*-lun-?"
 }
-while [ "$(find_devs | wc -l)" -ne "${DISK_COUNT}" ]
-do
-  logger -p local0.notice \
-  "Discovered new virtual devices in \"/dev\" [$(find_devs | wc -l)/${DISK_COUNT}]"
-done
 
-logger -p local0.info \
-"Ready ISCSI virtual devices in \"/dev\" [$(find_devs | wc -l)/${DISK_COUNT}]"
+
+function waitfor_devs()
+{
+  while [ "$(find_devs | wc -l)" -ne "${DISK_COUNT}" ]
+  do
+    logger -p local0.notice \
+    "Discovered new virtual devices in \"/dev\" [$(find_devs | wc -l)/${DISK_COUNT}]"
+  done
+
+  logger -p local0.info \
+  "Ready ISCSI virtual devices in \"/dev\" [$(find_devs | wc -l)/${DISK_COUNT}]"
+}
+
 
 # format & partition disk
-# INITIATOR_IQN=$(iscsi-iname | cut -d ':' -f 1)
-# INITIATOR_ADDRESS='127.0.0.1'
-for i in $(seq 1 ${DISK_COUNT})
-do
-  lun_name="lun$((${i} - 1))"
-  
-  # create label
-  sudo parted -s /dev/disk/by-path/ip-${TARGET_ADDRESS}\:${TARGET_PORT}-iscsi-${TARGET_IQN}\:${lun_name}-lun-1 mklabel msdos
-  sudo parted -s /dev/disk/by-path/ip-${TARGET_ADDRESS}\:${TARGET_PORT}-iscsi-${TARGET_IQN}\:${lun_name}-lun-1 unit % mkpart primary ext4 0 100
-  sudo partprobe -s && sleep 1s
-  sudo mkfs -t ext4 /dev/disk/by-path/ip-${TARGET_ADDRESS}\:${TARGET_PORT}-iscsi-${TARGET_IQN}\:${lun_name}-lun-1-part1
-done
+function add_partitions()
+{
+  for i in $(seq 1 ${DISK_COUNT})
+  do
+    lun_name="lun$((${i} - 1))"
+    
+    # create label
+    sudo parted -s /dev/disk/by-path/ip-${TARGET_ADDRESS}\:${TARGET_PORT}-iscsi-${TARGET_IQN}\:${lun_name}-lun-1 mklabel msdos
+    sudo parted -s /dev/disk/by-path/ip-${TARGET_ADDRESS}\:${TARGET_PORT}-iscsi-${TARGET_IQN}\:${lun_name}-lun-1 unit % mkpart primary ext4 0 100
+    sudo partprobe -s && sleep 1s
+    sudo mkfs -t ext4 /dev/disk/by-path/ip-${TARGET_ADDRESS}\:${TARGET_PORT}-iscsi-${TARGET_IQN}\:${lun_name}-lun-1-part1
+  done
 
-# wait a few seconds to mount the device and verify
-lsblk
+  lsblk
+}
+
+configure_target
+configure_initiator
+waitfor_devs
+add_partitions
+
