f35e8834254ceebb60e8adfc20f4b614b33a8856
Florian JUDITH <florian.judith.b@gmail.com>
2023-02-20 00:09:00 -0500

    Cleaned variable evaluation

--- LFS-essential-commands/01-log-into-local-remote-graphical-and-text-mode-consoles/assets/scripts/install_ssh.sh
+++ LFS-essential-commands/01-log-into-local-remote-graphical-and-text-mode-consoles/assets/scripts/install_ssh.sh
@@ -12,26 +12,24 @@ function generate_keys()
 {
     USER_NAME=$(id -un)
     HOST_NAME=$(hostname)
+    SSH_KEY_FILE="/root/.ssh/id_rsa"
     if [ ! "$(id -un)" = "root" ]
     then
         SSH_KEY_FILE="${HOME}/.ssh/id_rsa"
-    else
-        SSH_KEY_FILE="/root/.ssh/id_rsa"
     fi
     
 
-    if [ -f "${SSH_KEY_FILE}" ]; then
+    if [ ! -f "${SSH_KEY_FILE}" ]; then
         ssh-keygen -f "${SSH_KEY_FILE}" -C "${USER_NAME}@${HOST_NAME}" -q -N ''
     fi
 }
 
 function udpate_user_data()
 {
+    SSH_PUB_KEY_FILE="/root/.ssh/id_rsa.pub"
     if [ ! "$(id -un)" = "root" ]
     then
         SSH_PUB_KEY_FILE="${HOME}/.ssh/id_rsa.pub"
-    else
-        SSH_PUB_KEY_FILE="/root/.ssh/id_rsa.pub"
     fi
     
     if [ -f "/tmp/default-user-data.yml" ] && [ -f "${SSH_PUB_KEY_FILE}" ]; then
