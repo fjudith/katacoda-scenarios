a2b30c4263df51880c6297237455210d78b9a09a
Florian JUDITH <florian.judith.b@gmail.com>
2023-02-20 00:13:05 -0500

    Updated container name

--- LFS-essential-commands/01-log-into-local-remote-graphical-and-text-mode-consoles/assets/scripts/install_lxd.sh
+++ LFS-essential-commands/01-log-into-local-remote-graphical-and-text-mode-consoles/assets/scripts/install_lxd.sh
@@ -28,7 +28,7 @@ function start_containers()
 
     if [ "$(id -un)" = "root" ]
     then
-        CONTAINER_NAME='host02'
+        CONTAINER_NAME='lxd01'
         lxc launch "${DISTRIBUTION,,}:${RELEASE}" "${CONTAINER_NAME}" --config=user.user-data="$(cat /tmp/default-user-data.yml)"
     fi
 }
