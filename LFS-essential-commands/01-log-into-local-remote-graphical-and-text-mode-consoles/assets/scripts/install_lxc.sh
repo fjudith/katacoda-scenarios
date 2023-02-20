bf7df52f400da57781ff1d3f6dfa98b07bb5cad4
Florian JUDITH <florian.judith.b@gmail.com>
2023-02-19 19:44:05 -0500

    Added lxc container create

--- LFS-essential-commands/01-log-into-local-remote-graphical-and-text-mode-consoles/assets/scripts/install_lxc.sh
+++ LFS-essential-commands/01-log-into-local-remote-graphical-and-text-mode-consoles/assets/scripts/install_lxc.sh
@@ -5,6 +5,8 @@ function install_dependencies()
 {
     sudo apt-get update -y \
     && apt-get install -y \
+       'lsb-release' \
+       'openssh-server' \
        'lxc' \
        'lxd' \
        'python3-pylxd' \
@@ -13,6 +15,7 @@ function install_dependencies()
 
 function configure_veth()
 {
+    lxc-checkconfig
     echo "$(id -un) veth lxcbr0 10" | sudo tee -a /etc/lxc/lxc-usernet
 }
 
@@ -26,10 +29,26 @@ function configure_lxc()
     MS_GID="$(grep "$(id -un)" /etc/subgid  | cut -d : -f 2)"
     ME_GID="$(grep "$(id -un)" /etc/subgid  | cut -d : -f 3)"
 
-    echo "lxc.idmap = u 0 $MS_UID $ME_UID" >> ~/.config/lxc/default.conf
-    echo "lxc.idmap = g 0 $MS_GID $ME_GID" >> ~/.config/lxc/default.conf
-
+    if [ "$(id -un)" = "root" ]
+    then
+        echo "lxc.idmap = u 0 $MS_UID $ME_UID" | sudo tee -a /etc/lxc/default.conf
+        echo "lxc.idmap = g 0 $MS_GID $ME_GID" | sudo tee -a /etc/lxc/default.conf
+    else
+        echo "lxc.idmap = u 0 $MS_UID $ME_UID" | tee -a ~/.config/lxc/default.conf
+        echo "lxc.idmap = g 0 $MS_GID $ME_GID" | tee -a ~/.config/lxc/default.conf
+    
+    fi
     export DOWNLOAD_KEYSERVER="hkp://keyserver.ubuntu.com"
+}
+
+function start_containers()
+{
+    if [ "$(id -un)" = "root" ]
+    then
+        sudo lxc-create --template download --name host02 -- --dist ubuntu --release DISTRO-SHORT-CODENAME --arch amd64
+    else
+        systemd-run --unit=my-unit --user --scope -p "Delegate=yes" -- lxc-create --template download --name host02 -- --dist ubuntu --release DISTRO-SHORT-CODENAME --arch amd64
+    fi
 }
 
 install_dependencies
