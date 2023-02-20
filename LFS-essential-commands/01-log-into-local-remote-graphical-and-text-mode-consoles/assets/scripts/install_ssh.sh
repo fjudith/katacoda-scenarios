e27d57cbeae182f5296aa978569be8431af02110
Florian JUDITH <florian.judith.b@gmail.com>
2023-02-19 23:40:05 -0500

    Fixed typos

--- LFS-essential-commands/01-log-into-local-remote-graphical-and-text-mode-consoles/assets/scripts/install_ssh.sh
+++ LFS-essential-commands/01-log-into-local-remote-graphical-and-text-mode-consoles/assets/scripts/install_ssh.sh
@@ -11,6 +11,7 @@ function generate_keys()
     USER_NAME=$(id -un)
     HOST_NAME=$(hostname)
     SSH_KEY_FILE="${HOME}/.ssh/id_rsa_${USER_NAME}"
+    SSH_PUB_KEY_FILE="${HOME}/.ssh/id_rsa_${USER_NAME}.pub"
 
     if [ -f "${SSH_KEY_FILE}" ]; then
         ssh-keygen -f "${SSH_KEY_FILE}" -C "${USER_NAME}@${HOST_NAME}" -q -N ''
@@ -22,7 +23,7 @@ function udpate_user_data()
     SSH_KEY_FILE="${HOME}/.ssh/id_rsa_${USER_NAME}"
     
     if [ -f "/tmp/default-user-data.yml" ]; then
-        sed -i "s/__USER_SSH_PUBLIC_KEY__/$(cat $SSH_KEY_FILE)/g" /tmp/default-user-data.yml
+        sed -i "s/__USER_SSH_PUBLIC_KEY__/$(cat $SSH_PUB_KEY_FILE)/g" /tmp/default-user-data.yml
     fi
 }
 
